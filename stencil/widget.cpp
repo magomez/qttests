809b0badee424dcf732669eb316d5d5bd49f958c
Miguel Gomez <magomez@igalia.com>
2014-10-10 10:06:27 +0200

    Make stencil test fullscreen

--- stencil/widget.cpp
+++ stencil/widget.cpp
@@ -1,9 +1,11 @@
 #include "widget.h"
+#include <QDesktopWidget>
+#include <QApplication>
 
 Widget::Widget()
     : QGLWidget()
 {
-        setFixedSize(600, 400);
+    setFixedSize((QApplication::desktop()->screenGeometry().size()));
 }
 
 void Widget::paintEvent(QPaintEvent *event)
@@ -14,13 +16,13 @@ void Widget::paintEvent(QPaintEvent *event)
     painter.fillRect(0, 0, width(), height(), Qt::black); 
     
     painter.setFont(QFont("Arial", 30));
-    painter.drawText(0, 100, "metrological");
+    painter.drawText(0, 100, "this is 30px and fine");
 
     painter.setFont(QFont("Arial", 70));
-    painter.drawText(0, 200, "metrological");
+    painter.drawText(0, 200, "this is 70px and noisy");
 
     painter.setFont(QFont("Arial", 100));
-    painter.drawText(0, 350, "metrological");
+    painter.drawText(0, 400, "this is 100px and noisy");
 
     painter.end();
 }
